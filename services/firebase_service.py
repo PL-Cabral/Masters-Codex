15f105194a8115fd2a8c71530b53d1d69f06237d
Kelly-0W <bunnylady034@gmail.com>
2026-06-29 22:44:39 -0300

    melhorando os serviços do firebase

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,28 +1,116 @@
-import firebase_admin
-
 from firebase_admin import credentials
 from firebase_admin import db
+import firebase_admin
 
-# Caminho da chave privada do Firebase
-FIREBASE_KEY_PATH = "secrets/firebase_key.json"
+from core.table import Table
+from core.entity import Entity
+from core.users.user import User
 
-# Copie essa URL direto do seu Realtime Database no Firebase Console
-DATABASE_URL = "https://masters-codex-default-rtdb.firebaseio.com/"
+FIREBASE_CREDENTIALS_PATH = "secrets/firebase_key.json"
 
-# Inicializa o Firebase apenas uma vez
+DATABASE_URL = ("https://masters-codex-default-rtdb.firebaseio.com/")
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate(FIREBASE_KEY_PATH)
+    cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
 
-    firebase_admin.initialize_app(cred, {
-        "databaseURL": DATABASE_URL
-    })
+    firebase_admin.initialize_app(
+        cred,
+        {
+            "databaseURL": DATABASE_URL
+        }
+    )
 
-def save_entity(table_id, entity):
-    entity.table_id = table_id
+def save_user(user):
+    ref = db.reference(
+        f"users/{user.id}"
+    )
 
-    data = entity.to_dict()
+    ref.set(
+        user.to_dict()
+    )
 
-    ref = db.reference(f"tables/{table_id}/entities/{entity.id}")
-    ref.set(data)
+    return user.id
+
+def get_user(user_id):
+    ref = db.reference(
+        f"users/{user_id}"
+    )
+
+    data = ref.get()
+
+    if data is None:
+        return None
+
+    return data
+
+def delete_user(user_id):
+    ref = db.reference(
+        f"users/{user_id}"
+    )
+
+    ref.delete()
+
+def save_table(table):
+    ref = db.reference(
+        f"tables/{table.id}"
+    )
+
+    ref.set(
+        table.to_dict()
+    )
+
+    return table.id
+
+def get_table(table_id):
+    ref = db.reference(
+        f"tables/{table_id}"
+    )
+
+    data = ref.get()
+
+    if data is None:
+        return None
+
+    return Table.from_dict(data)
+
+def delete_table(table_id):
+    ref = db.reference(
+        f"tables/{table_id}"
+    )
+
+    ref.delete()
+
+def save_entity(entity):
+    ref = db.reference(
+        f"entities/{entity.id}"
+    )
+
+    ref.set(
+        entity.to_dict()
+    )
 
     return entity.id
+
+def get_entity(entity_id):
+    ref = db.reference(
+        f"entities/{entity_id}"
+    )
+
+    data = ref.get()
+
+    if data is None:
+        return None
+
+    return data
+
+def delete_entity(entity_id):
+    ref = db.reference(
+        f"entities/{entity_id}"
+    )
+
+    ref.delete()
+
+def ping_database():
+    ref = db.reference("/")
+
+    return ref.get()
